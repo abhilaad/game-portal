b20a5ef047221add4362f32c15934c6c47b9b62d
abhilaad <abhishek.24042013@gmail.com>
2023-06-11 19:44:32 +0530

    games and platform implementation via react query

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "0ea8fa7ddb4743a386353b183fd98a15",
+    key: "0ea8fa7ddb4743a386353b183fd98a15s",
   },
 });
