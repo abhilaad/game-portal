b3473cac41af5d3977f44540c0eb97c4b48d3963
abhilaad <abhishek.24042013@gmail.com>
2023-06-12 23:38:56 +0530

    refactoring genre and platform query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,5 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import ms from 'ms';
 import { GameQuery } from '../App';
 import APIClient, {
   FetchResponse,
@@ -22,17 +23,17 @@ const useGames = (gameQuery: GameQuery) =>
     queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
-          genres: gameQuery.genre?.id,
-          parent_platforms: gameQuery.platform?.id,
+          genres: gameQuery.genreId,
+          parent_platforms: gameQuery.platformId,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
-          page: pageParam
+          page: pageParam,
         },
       }),
     getNextPageParam: (lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000 //24h
+    staleTime: ms('24h')
   });
 
 export default useGames;
